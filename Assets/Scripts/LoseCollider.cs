2e59c79e8b942cd95d7fbd22ba300e7059dfbdbc
Cfranc7 <chrisfmichaels@gmail.com>
2016-02-06 16:54:15 -0800

    Adds dynamic level and lives text

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -5,6 +5,7 @@ public class LoseCollider : MonoBehaviour {
 
 	private Camera camera;
 	private LevelManager levelManager;
+	private TextController textController;
 	private Ball ball;
 	public int livesLeft = 3;
 	public bool debug = true;
@@ -15,6 +16,7 @@ public class LoseCollider : MonoBehaviour {
 	void Start () {
 		// Link the level mgr with the lose collider
 		levelManager = Object.FindObjectOfType<LevelManager>();	
+		textController = GameObject.FindObjectOfType<TextController>();
 		camera = Object.FindObjectOfType<Camera>();	
 	}
 
@@ -27,6 +29,7 @@ public class LoseCollider : MonoBehaviour {
 		if (livesLeft > 0) {
 			livesLeft--;
 			camera.LoseLifeBg();
+			textController.SetLivesText(livesLeft);
 			if (debug) Debug.Log ("Lives left: " + livesLeft);
 		} 
 		
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	
+	public void SetLivesText (int livesLeft) {
+		text.text = "Lives Remaining: " + livesLeft;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
