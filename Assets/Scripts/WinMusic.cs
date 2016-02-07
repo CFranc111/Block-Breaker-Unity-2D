ca06528493928f083b3931a52856d7e168e9ffc8
Cfranc7 <chrisfmichaels@gmail.com>
2016-02-07 09:22:44 -0800

    Adds music on game win

---
+++ Assets/Scripts/WinMusic.cs
@@ -0,0 +1,24 @@
+﻿// Script courtesy of Tuncer @answers.unity3d.com
+using UnityEngine;
+using System.Collections;
+
+public class WinMusic : MonoBehaviour {
+
+		public AudioClip gameWinClip;
+		public AudioClip bgLoopClip;
+		
+		void Start()
+		{
+			GetComponent<AudioSource>().loop = true;
+			StartCoroutine(playClips());
+		}
+		
+		IEnumerator playClips()
+		{
+			audio.clip = gameWinClip;
+			audio.Play();
+			yield return new WaitForSeconds(audio.clip.length);
+			audio.clip = bgLoopClip;
+			audio.Play();
+		}
+}
